f3254392f6bd99c82564e4df7e80f9155146b7a9
devLee <ezequiel.github33@gmail.com>
2024-11-13 08:08:17 -0300

    Projeto finalizado

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Bikcraft! <img src="./private/favicon.svg" width="24" height="24">
+## Projeto acadêmico
+
+## Resumo
+
+Temos um site interativo e responsivo de uma loja de bicicletas com várias páginas e funcionalidades, também foi usado o Animate.css e o wow.js para leves animações e com algumas modificações para melhor experiência do usuário. Projeto desenvolvido em HTML, CSS e JavaScript.<br>
+(Projeto acadêmico)
+
+## Tecnologias utilizadas
+
+<div style="display: inline_block;">
+    <img height="40" width="40" alt="html5" src="./private/html5.png"/>
+    <img height="40" width="40" alt="css3" src="./private/css3.png"/>
+    <img height="40" width="40" alt="JavaScript" src="./private/javascript.png"/>
+</div>
+
+## Imagem completa do site
+### Desktop
+<img src="./private/bikcraft-desktop.png">
+
+### Mobile
+<img src="./private/bikcraft-mobile.png">
+
+## Acesse o Projeto
+
+Você pode acessar o projeto através do link abaixo:
+
+➜ [Link do projeto Lobo](https://ezequiel-lee.github.io/bikcraft/)
+
+## Agradecimentos
+
+(Agradecimentos ao André da Origamid pelo conhecimento.)
---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Ativa links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativaLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add('ativo');
+    }
+}
+
+links.forEach(ativaLink);
+
+
+// ativar links do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto)
+
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPergunta(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPergunta);
+
+
+// Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 768px)").matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
